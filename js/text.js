23d8e79ed0d747f324dca61550a3e593f0653d9b
Andrew Gibson <gibson_andrew@yahoo.com>
2012-09-02 20:39:48 -0400

    reorder text alphabetically

--- js/text.js
+++ js/text.js
@@ -10,46 +10,6 @@
         "en" : "close"
         ,"fr" : "fermer"
       }
-      ,"print" : {
-        "en" : "Print"
-        ,"fr" : "Imprime"
-      }
-      ,"print_help" : {
-        "en" : "Help text here, for long tables, use landscape/legal"
-        ,"fr" : "fr"
-      }
-      ,"title" : {
-        "en": "Lape and Expenditure Statistics"
-        ,"fr": "Statistiques sur les fonds inutilisés et les dépenses"
-      }
-      ,"welcome" : {
-        "en": "Search above for a  Department to get started"
-        ,"fr": "choisis un ministére"
-      }
-      ,"loading" : {
-        "en": "Loading"
-        ,"fr": "Chargement"
-      }
-      ,"sub_total": {
-        "en": "Subtotal"
-        ,"fr": "Sous Total"
-      }
-      ,"total": {
-        "en": "Total"
-        ,"fr": "Total"
-      }
-      ,"sub_avg": {
-        "en": "Average"
-        ,"fr": "Moyen"
-      }
-      ,"copy" : {
-        "en" : "Copy table for pasting"
-        ,"fr": "francais"
-      }
-      ,"print_friend" : {
-        "en" : "Printer Friendly"
-        ,"fr" : "Imprimer"
-      }
       ,"copy_tooltip" : {
         "en" : "Clicking this button will load the paste buffer with the table. You can then switch over to Excel and paste in the table"
         ,"fr" : "fr:Clicking this button will load the paste buffer with the table. You can then switch over to Excel and paste in the table"
@@ -65,6 +25,62 @@
       ,"full_screen" : {
         "en" : "Full Screen"
         ,"fr": "Plein Ecran"
+      }
+      ,"graphs" : {
+        "en" : "Graphs"
+        ,"fr" : "Graphiques"
+      }
+      ,"lang" : {
+        "en" : "Français"
+        ,'fr' : 'English'
+      }
+      ,"loading" : {
+        "en": "Loading"
+        ,"fr": "Chargement"
+      }
+      ,"print" : {
+        "en" : "Print"
+        ,"fr" : "Imprime"
+      }
+      ,"print_help" : {
+        "en" : "Help text here, for long tables, use landscape/legal"
+        ,"fr" : "fr"
+      }
+      ,"print_friend" : {
+        "en" : "Printer Friendly"
+        ,"fr" : "Imprimer"
+      }
+      ,"sub_avg": {
+        "en": "Average"
+        ,"fr": "Moyen"
+      }
+      ,"sub_total": {
+        "en": "Subtotal"
+        ,"fr": "Sous Total"
+      }
+      ,"title" : {
+        "en": "Lape and Expenditure Statistics"
+        ,"fr": "Statistiques sur les fonds inutilisés et les dépenses"
+      }
+      ,'table' : {
+        'en' : 'Table'
+        ,'fr' : 'Tableau'
+      }
+      ,'tables' : {
+        'en' : 'Tables'
+        ,'fr' : 'Tableaux'
+      }
+      ,"total": {
+        "en": "Total"
+        ,"fr": "Total"
+      }
+      ,"copy" : {
+        "en" : "Copy table for pasting"
+        ,"fr": "fr:Copy table for pasting"
+      }
+      ,"welcome" : {
+        "en": "Search above for a  Department to get started"
+        ,"fr": "choisis un ministére"
       }
     }
     LANG.l = function(entry,lang){
