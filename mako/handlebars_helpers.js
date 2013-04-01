948ce520480caed98318b914af8111de0341e231
Andrew Gibson <gibson_andrew@yahoo.com>
2013-04-01 12:29:19 -0400

    split the mako templates apart for code re-use and enabling the static open data pages

---
+++ mako/handlebars_helpers.js
@@ -0,0 +1,25 @@
+Handlebars.registerHelper("gt",function(context){
+  return app.get_text(context);
+});
+Handlebars.registerHelper("uri",function(context){
+  return encodeURI(context[app.state.get("lang")]);
+});
+Handlebars.registerHelper("lang",function(context){
+  context[app.state.get("lang")];
+  return context[app.state.get("lang")];
+});
+Handlebars.registerHelper("encodeURI",function(context,options){
+  if (_.has(context,'en') && _.has(context,"fr")){
+    context = context[app.state.get("lang")];
+  }
+  return encodeURI(context);
+});
+Handlebars.registerHelper("is_dept",function(context,options){
+  var dept = context['dept'][app.state.get("lang")];
+  if (_.has(context,'org_type') && context['org_type']['en'] === 'Departments') {
+    return options.fn({dept:dept});
+  }else{
+    return options.inverse({dept:dept});
+  }
+});
+
