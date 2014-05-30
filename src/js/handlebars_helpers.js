44ed7012dcc9935f4fc9fc8342cfb150eaa066ab
Andrew Gibson <gibson_andrew@yahoo.com>
2014-05-30 14:40:41 -0400

    add new handlebars helpers for number changes

--- src/js/handlebars_helpers.js
+++ src/js/handlebars_helpers.js
@@ -1,4 +1,29 @@
 ns().APP.dispatcher.once("init", function(app){
+
+  Handlebars.registerHelper("change_by",function(context){
+     if (_.head(context)==='-'){
+      return "decreased by " + context;
+     } else {
+      return "increased by " + context;
+     }
+  });
+
+  Handlebars.registerHelper("change_de_m",function(context){
+     if (_.head(context)==='-'){
+      return "diminés de " + context;
+     } else {
+      return "augmentés de " + context;
+     }
+  });
+
+  Handlebars.registerHelper("change_de_f",function(context){
+     if (_.head(context)==='-'){
+      return "diminées de " + context;
+     } else {
+      return "augmentées de " + context;
+     }
+  });
+
   Handlebars.registerHelper("gt",function(context){
     return app.get_text(context);
   });
@@ -23,8 +48,8 @@ ns().APP.dispatcher.once("init", function(app){
     return encodeURI(context);
   });
   Handlebars.registerHelper("is_dept",function(context,options){
-    var dept = context['dept'][app.state.get("lang")];
-    if (_.has(context,'org_type') && context['org_type']['en'] === 'Departments') {
+    var dept = context.dept[app.state.get("lang")];
+    if (_.has(context,'org_type') && context.org_type.en === 'Departments') {
       return options.fn({dept:dept});
     }else{
       return options.inverse({dept:dept});
