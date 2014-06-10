52d9909b41fcbf7f8c68973b17802de7ca792b58
Andrew Gibson <gibson_andrew@yahoo.com>
2014-06-10 16:50:38 -0400

    transform line graph to be re-runable and to fix the axis tick marks along with centering the labels

--- src/js/d3/line.js
+++ src/js/d3/line.js
@@ -5,31 +5,42 @@
 
     LINE.ordinal_line = D3.extend_base(function(svg,index){
       /* data in the format of 
-      *  data = { "series 1" : [y1,y2,y3],
+      *  series = { "series 1" : [y1,y2,y3],
           *     "series 2" : [y1,y2,y3]}
       *  ticks = ["tick1","tick2"."tick3"]
       */
-      var series = d3.keys(this.series),
-          add_xaxis = this.add_xaxis,
-          add_yaxis = this.add_yaxis,
+      var html = this.html,
+          add_xaxis =  this.add_xaxis  === undefined ? true : this.add_xaxis, 
+          add_yaxis =  this.add_yaxis  === undefined ? true : this.add_yaxis,   
           x_axis_line = this.x_axis_line  === undefined ? true : this.x_axis_line,
-          add_legend = this.add_legend,
+          add_legend =  this.add_legend  === undefined ? true : this.add_legend,
           add_labels = this.add_labels,
-          html_ticks = this.html_ticks,
           add_under_area  = this.add_under_area || false,
           formater  = this.formater,
           colors = this.colors || D3.tbs_color(),
           title = this.title,
-          top_margin = add_legend ? series.length  * 20 + 15 : 20,
-          margin = this.margin || {top: top_margin, 
+          margin = this.margin || {top: 20, 
                                     right: 20, 
                                     bottom: 30, 
                                     left: 50},
           height = this.height - margin.top - margin.bottom,
           width = this.width - margin.left - margin.right,
           y_axis = this.y_axis || '',
-          ticks = this.ticks,
+          graph_area  = svg
+                .attr({
+                  width : width+margin.left+margin.right,
+                  height : height+margin.top+margin.bottom})
+                .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+     D3.add_hatching(graph_area,width,height);
+
+
+     this.render = function(options){
+       _.extend(this, options);
+      var series = d3.keys(this.series),
           values = d3.values(this.series),
+          ticks = this.ticks,
           data = _.map(ticks,function(tick,i){
             return {tick : tick,
                     data :  _.map(series, function(serie){
@@ -37,193 +48,200 @@
                     },this)
             };
           },this),
-          extent = d3.extent(d3.merge(values)),
-          y_bottom = extent[0] > 0 ? 0 : 1.1 * extent[0],
-          y_top = extent[1] < 0 ? 0 : 1.1 * extent[1],
-          zero = 0,
           x = d3.scale.ordinal()
             .domain(ticks)
             .rangePoints([0, width], 0.1),
-          point_width = x.range()[1] -x.range()[0],
+          x_range = x.range(),
+          tick_width = x_range[1]- x_range[0],
+          extent = d3.extent(d3.merge(values)),
+          zero = 0,
+          y_bottom = extent[0] > 0 ? 0 : 1.1 * extent[0],
+          y_top = extent[1] < 0 ? 0 : 1.1 * extent[1],
           y = d3.scale.linear()
             .domain([y_bottom, y_top])
-            .range([height, 0]),
-          xAxis = d3.svg.axis()
-            .scale(x)
-            .tickPadding(5)
-            .orient("bottom"),
-          /*
-          * setup the main graph area and add the bars
-          * set up the axes  
-          */
-          html = this.html,
-      graph_area  = svg
+            .range([height, 0]);
+     
+          // add the title
+          svg.select("text.title").remove();
+          svg.append("text")
             .attr({
-              width : width+margin.left+margin.right,
-              height : height+margin.top+margin.bottom})
-            .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-     D3.add_hatching(svg,width+margin.left+margin.right,height+margin.top+margin.bottom);
-
-      // add the title
-      svg.append("text")
-        .attr({
-          "class" : "title",
-          "x" : margin.left + width/2,
-          "y" : 12,
-        })
-        .style({
-          "text-anchor" : "middle",
-          "font-size" : "12px",
-          "font-weight" : "600"
-        })
-        .text(title);
-
-      if (add_legend){
-        make_legend(svg,series,width+margin.left, colors);
-      }
-
-      if (add_xaxis){
-        graph_area.append("g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + height+ ")")
-              .call(xAxis);
-        if (html_ticks){
-          graph_area.select(".x.axis").selectAll(".tick text").remove();
-          html.selectAll("div.tick")
-            .data(ticks)
-            .enter()
-            .append("div")
-            .attr("class","tick")
+              "class" : "title",
+              "x" : margin.left + width/2,
+              "y" : 12,
+            })
             .style({
-              "position" : "absolute",
+              "text-anchor" : "middle",
               "font-size" : "12px",
-              "text-weight" : "bold",
-              "top" : height+margin.top+10+"px",
-              "width": point_width+"px",
-              "left"  : function(d) {return x(d)+margin.left+"px" ; }
+              "font-weight" : "600"
             })
-            .html(function(d){ return d;});
-        }
-        if (!x_axis_line){
-          graph_area.select(".x.axis path").remove();
-        }
-      }
+            .text(title);
 
-      if (add_yaxis){
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .ticks(10)
-            .tickFormat(formater)
-            .orient("left");
+          if (add_legend){
+            make_legend(svg,series,width+margin.left, colors);
+          }
 
-        graph_area.append("g")
-              .attr("class", "y axis")
-              .call(yAxis)
-              .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text(y_axis);
-      }
+          if (add_xaxis){
 
-      if (add_labels){
-        html.selectAll("div.labels")
-          .data(data)
-          .enter()
-          .append("div")
-          .attr("class","labels")
-          .style({
-            "position" : "absolute",
-            "top" : "0px",
-            "height" : "10px",
-            "width": point_width+"px",
-            "left"  : function(d) {return x(d.tick)+margin.left+"px" ; }
-          })
-          .selectAll("div.label")
-          .data(function(d){ return d.data;})
-          .enter()
-          .append("div")
-          .attr("class","label")
-          .html(function(d){ return formater(d.value);})
-          .style({
-            "text-align": "center",
-            "position" : "absolute",
-            "text-weight" : "bold",
-            "font-size" : "12px",
-            "height" : "10px",
-            "top"  : function(d){
-              if (d.value === 0){
-                return  y(d.value)+"px";
-              }
-              else if (d.value > 0){
-                return margin.top - 12 + y(d.value) - 5+"px";
-              } else {
-                return y(d.value) +20+ "px";
-              }
+            var xAxis = d3.svg.axis()
+              .scale(x)
+              .outerTickSize(0)
+              .tickPadding(5)
+              .orient("bottom");
+
+            var xaxis_node = graph_area.select(".x.axis");
+            if (!xaxis_node.node()){
+              xaxis_node = graph_area.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height+ ")");
             }
-          });
-        html.selectAll("div.labels")
-          .data(data)
-          .exit()
-          .remove();
-      }
+            xaxis_node.call(xAxis);
 
-      _.each(series, function(serie, i){
+            graph_area.select(".x.axis").selectAll(".tick text").remove();
+            html.selectAll("div.tick")
+              .data(ticks)
+              .enter()
+              .append("div")
+              .attr("class","tick")
+              .style({
+                "position" : "absolute",
+                "text-align" : "center",
+                "font-size" : "12px",
+                "text-weight" : "bold",
+                "top" : height+margin.top+10+"px",
+                "width": tick_width+"px",
+                "left"  : function(d) {return x(d)-tick_width/2+margin.left+"px" ; }
+              })
+              .html(function(d){ return d;});
 
-        var yfunc = function(d){return y(d.data[i].value);};
-        var xfunc = function(d){return x(d.tick);} ;
+            if (!x_axis_line){
+              graph_area.select(".x.axis path").remove();
+            }
+          }
 
-        var line = d3.svg.line()
-          .x(xfunc)
-          .y(yfunc);
+          if (add_yaxis) {
 
-        var path = graph_area.append("path")
-          .datum(data)
-          .style({
-            "fill" : "none",
-            "stroke" : colors(i),
-            "stroke-width" : "3px"
-          })
-          .attr("d", line);
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .ticks(10)
+                .outerTickSize(0)
+                .tickFormat(formater)
+                .orient("left");
 
-        if (add_under_area){
+            var yaxis_node = graph_area.select(".y.axis");
+            if (!yaxis_node.node()){
+              yaxis_node = graph_area.append("g").attr("class", "y axis");
 
-          var area = d3.svg.area()
-            .x(xfunc)
-            .y0(height)
-            .y1(yfunc);
+              yaxis_node
+                  .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end")
+                    .text(y_axis);
+            }
+            yaxis_node.call(yAxis);
 
-          var under_area = graph_area.append("path")
-            .datum(data)
-            .style({
-              "fill" : colors(i),
-              "fill-opacity" : "0.2"
+          }
+
+          if (add_labels){
+            var labels = html.selectAll("div.labels")
+                            .data(data);
+            //labels
+            //  .enter()
+            //  .append("div")
+            //  .attr("class","labels")
+            //  .style({
+            //    "position" : "absolute",
+            //    "top" : "0px",
+            //    "height" : "10px",
+            //    "width": point_width+"px",
+            //    "left"  : function(d) {return x(d.tick)+margin.left+"px" ; }
+            //  })
+            //  .selectAll("div.label")
+            //  .data(function(d){ return d.data;})
+            //  .enter()
+            //  .append("div")
+            //  .attr("class","label")
+            //  .html(function(d){ return formater(d.value);})
+            //  .style({
+            //    "text-align": "center",
+            //    "position" : "absolute",
+            //    "text-weight" : "bold",
+            //    "font-size" : "12px",
+            //    "height" : "10px",
+            //    "top"  : function(d){
+            //      if (d.value === 0){
+            //        return  y(d.value)+"px";
+            //      }
+            //      else if (d.value > 0){
+            //        return margin.top - 12 + y(d.value) - 5+"px";
+            //      } else {
+            //        return y(d.value) +20+ "px";
+            //      }
+            //    }
+            //  });
+            labels
+              .exit()
+              .remove();
+          }
+
+          // draw the line for each series and add the dots
+          _.each(series, function(serie, i){
+
+            var yfunc = function(d){return y(d.data[i].value);};
+            var xfunc = function(d){return x(d.tick);} ;
+
+            var line = d3.svg.line()
+              .x(xfunc)
+              .y(yfunc);
+
+            var path = graph_area.append("path")
+              .datum(data)
+              .style({
+                "fill" : "none",
+                "stroke" : colors(i),
+                "stroke-width" : "3px"
+              })
+              .attr("d", line);
+
+            if (add_under_area){
+
+              var area = d3.svg.area()
+                .x(xfunc)
+                .y0(height)
+                .y1(yfunc);
+
+              var under_area = graph_area.append("path")
+                .datum(data)
+                .style({
+                  "fill" : colors(i),
+                  "fill-opacity" : "0.2"
+                })
+                .attr("d", area);
+            }
+
+            var dots = graph_area.selectAll("circle.dots"+i)
+                .data(data);
+            dots.exit().remove();
+            dots
+              .enter()
+              .append("circle")
+              .attr({
+                "class" : "dots"+i,
+                "cy" : yfunc,
+                "cx" : xfunc,
+                "r" : "10"
             })
-            .attr("d", area);
-        }
+            .style({
+              "fill" :colors(i),
+              "fill-opacity": "0.8",
+              "stroke" : "white",
+              "stroke-width" : "3px"
+            });
+          });
+     };
 
-        var dots = graph_area.selectAll("circle.dots"+i)
-            .data(data);
-        dots.exit().remove();
-        dots
-          .enter()
-          .append("circle")
-          .attr({
-            "class" : "dots"+i,
-            "cy" : yfunc,
-            "cx" : xfunc,
-            "r" : "10"
-        })
-        .style({
-          "fill" : "steelblue",
-          "fill-opacity": "0.8",
-          "stroke" : "white",
-          "stroke-width" : "3px"
-        });
-      });
+     this.render({});
    });
 
    function make_legend(sel,legend,width,colors){
