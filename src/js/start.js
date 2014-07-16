d7f5529c749a74e91029e2c319f9edfd62387c90
Andrew Gibson <gibson_andrew@yahoo.com>
2014-07-16 15:14:05 -0400

    removed jqmobile dependency

--- src/js/start.js
+++ src/js/start.js
@@ -1,8 +1,4 @@
 $(function(){
-    // jquerymobile is bundled with WET
-    // want to disable the jqmobile routing
-    // functionality
-    $.mobile.hashListeningEnabled = false;
     // WET adds this by default, for WCAG there can
     // only be one h1
     $('h1').remove();
