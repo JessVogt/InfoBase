30d3476d9c58e3e26713dbb0c8f354c775e41fe2
Andrew Gibson <gibson_andrew@yahoo.com>
2014-07-18 16:37:59 -0400

    initial commit of branhc of inclusion of inventory data

---
+++ src/js/inventory.js
@@ -0,0 +1,9 @@
+(function() {
+  var APP = ns('APP');
+
+  // add the t-:org-:table" route, example:   #d-AGR or d-FO
+  APP.add_container_route("inventory","inventory",function(container, org,table){
+    debugger;
+  });
+
+})();
